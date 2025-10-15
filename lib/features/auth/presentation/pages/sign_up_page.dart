d904afb48a08946d3e3a7b0d13ab7adfeadccc83
unknown <samuelketema01@gmail.com>
2025-10-15 23:27:55 +0300

    Sign up page

--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weblog/features/auth/presentation/widgets/auth_field.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -11,7 +12,22 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'SignUp.',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20,),
+          AuthField(hintText: "name",),
+          SizedBox(height: 20,),
+          AuthField(hintText: "email",),
+          SizedBox(height: 20,),
+          AuthField(hintText: "password",)
+          
+        ],
+      ),
     );
   }
 }
