6bbce367ebe81226ef129169ace06b13ee4487ec
Samuel Ketema <samuelketema01@gmail.com>
2025-11-01 22:51:40 +0300

    Textfield customization

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
+  final bool isObscure;
   final TextEditingController controller;
-   final String hintText;
-  const AuthField( {super.key,  required this.hintText, required this.controller});
+  final String hintText;
+  const AuthField({
+    super.key,
+    this.isObscure = false,
+    required this.hintText,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-      decoration: InputDecoration(
-        hintText: hintText
-      ),
+    return TextFormField(
+      obscureText: isObscure,
+      decoration: InputDecoration(hintText: hintText),
       validator: (value) {
         if (value!.isEmpty) {
           return "$hintText is misssing";
@@ -19,6 +24,5 @@ class AuthField extends StatelessWidget {
         }
       },
     );
-    
   }
 }
