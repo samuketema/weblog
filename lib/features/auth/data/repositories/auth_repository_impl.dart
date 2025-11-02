7a1ca2850596e18367f16103525e0f7a69ed27fd
Samuel Ketema <samuelketema01@gmail.com>
2025-11-02 23:30:52 +0300

    implementaion of the repository on the domain layer

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'package:fpdart/src/either.dart';
+import 'package:weblog/core/error/exceptions.dart';
+import 'package:weblog/core/error/failure.dart';
+import 'package:weblog/features/auth/data/dataSource/auth_remote_datasource.dart';
+import 'package:weblog/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource remoteDatasource;
+
+  AuthRepositoryImpl(this.remoteDatasource);
+  @override
+  Future<Either<Failure, String>> signInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+     final userId = await remoteDatasource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    }on ServerException catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
