fa9eab6bfd552451c0d0fd6ef40b296e1c8779f3
Samuel Ketema <samuelketema01@gmail.com>
2025-11-02 22:24:01 +0300

    secret folder for the database

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const String anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuZ2p2ZGpsZmtub3Zobm9ldHpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwODA1OTcsImV4cCI6MjA3NzY1NjU5N30.e_OTK0gwVNbVCetZsUiDeTTvBOb9metVTSOuv-GvxYY";
+  static const String url = 'https://vngjvdjlfknovhnoetzr.supabase.co';
+}
