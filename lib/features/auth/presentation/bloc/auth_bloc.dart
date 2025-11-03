bcd497edd769c5c1322e5f5c87ea86041ae94d06
Samuel Ketema <samuelketema01@gmail.com>
2025-11-03 14:12:53 +0300

    Bloc integration for authentication

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weblog/features/auth/domain/usecase/user_sign_up.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+ final UserSignUp _userSignUp;
+  AuthBloc({required UserSignUp userSignUp})
+    : _userSignUp = userSignUp,
+      super(AuthInitial()) {
+    on<AuthSignUp>((event, emit)async {
+   final res = await  _userSignUp(
+        UserSignUpParams(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+
+      res.fold((l)=> emit(AuthFailure(message: l.toString()))
+      , (r)=> emit(AuthSuccess(uid: r)));
+    });
+  }
+}
