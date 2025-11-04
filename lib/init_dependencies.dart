a8545e47b6812c8658aa25aeadb110060e1367da
Samuel Ketema <samuelketema01@gmail.com>
2025-11-04 23:44:42 +0300

    New file for implementing dependency injection.

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,34 @@
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:weblog/core/secrets/app_secrets.dart';
+import 'package:weblog/features/auth/data/dataSource/auth_remote_datasource.dart';
+import 'package:weblog/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:weblog/features/auth/domain/repositories/auth_repository.dart';
+import 'package:weblog/features/auth/domain/usecase/user_sign_up.dart';
+import 'package:weblog/features/auth/presentation/bloc/auth_bloc.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    anonKey: AppSecrets.anonKey,
+    url: AppSecrets.url,
+  );
+
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+_initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDatasource>(
+    () => AuthRemoteDataSourceImpl(serviceLocator()),
+  );
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+  serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
+  
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(userSignUp: serviceLocator()),
+  );
+}
