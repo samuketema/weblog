9283aa7da672062e5e6eb72083dd5ea0c710095d
Samuel Ketema <samuelketema01@gmail.com>
2025-11-07 23:33:23 +0300

    Code fix

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Blog App',
       theme: AppTheme.darktheme,
       home: SignUpPage(),
     );
