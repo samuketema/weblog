f82c8c468831ad9d9e21709fd0b7f7f12a4e52aa
Samuel Ketema <samuelketema01@gmail.com>
2025-11-09 22:46:15 +0300

    applying return type of user entity

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,13 +1,14 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:weblog/core/error/failure.dart';
+import 'package:weblog/features/auth/domain/entities/user.dart';
 
 abstract interface class AuthRepository {
-  Future<Either<Failure, String>> signInWithEmailPassword({
+  Future<Either<Failure, User>> signInWithEmailPassword({
     required String email,
     required String password,
   });
 
-  Future<Either<Failure,String>>signUpWithEmailPassword(
+  Future<Either<Failure,User>>signUpWithEmailPassword(
    {required String name,
    required String email,
    required String password}
