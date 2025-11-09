774a7837c0a72dca5a8644895046c885a9d37e37
Samuel Ketema <samuelketema01@gmail.com>
2025-11-09 22:48:33 +0300

    argument type replaced with user entity

--- lib/features/auth/presentation/bloc/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -6,9 +6,9 @@ sealed class AuthState {}
 final class AuthInitial extends AuthState {}
 final class AuthLoading extends AuthState{}
 final class AuthSuccess extends AuthState{
-  final String uid;
+  final User user;
 
-  AuthSuccess({required this.uid});
+  AuthSuccess({required this.user});
 }
 final class AuthFailure extends AuthState{
   final String message;
