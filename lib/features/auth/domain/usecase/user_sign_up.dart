2be448fc502d6603d2a72b47152db9a1cc843d8d
Samuel Ketema <samuelketema01@gmail.com>
2025-11-09 22:48:01 +0300

    applying return type of userentity to the usecase

--- lib/features/auth/domain/usecase/user_sign_up.dart
+++ lib/features/auth/domain/usecase/user_sign_up.dart
@@ -1,16 +1,17 @@
 import 'package:weblog/core/error/failure.dart';
 import 'package:weblog/core/usecase/usecase.dart';
+import 'package:weblog/features/auth/domain/entities/user.dart';
 import 'package:weblog/features/auth/domain/repositories/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-class UserSignUp implements Usecase<String, UserSignUpParams> {
+class UserSignUp implements Usecase<User, UserSignUpParams> {
  final AuthRepository authRepository;
   UserSignUp(this.authRepository);
   @override
-  Future<Either<Failure, String>> call(UserSignUpParams Params) async {
+  Future<Either<Failure, User>> call(UserSignUpParams Params) async {
     return await authRepository.signUpWithEmailPassword(
       name: Params.name,
-      email: Params.email,
+      email: Params.email,  
       password: Params.password,
     );
   }
