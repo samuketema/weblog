951b21e42112e58507532b5e99ba458cb26b41d4
Samuel Ketema <samuelketema01@gmail.com>
2025-11-10 23:34:03 +0300

    custom widget for snakbar show.

---
+++ lib/core/utils/show_snakbar.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void showSnakbar(BuildContext context, String content){
+  ScaffoldMessenger.of(context)..hideCurrentSnackBar()..showSnackBar(SnackBar(content: Text(content)));
+}
