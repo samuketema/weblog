8be7e0538c644bae77fae5cbccd3973d2acb3cd8
Samuel Ketema <samuelketema01@gmail.com>
2025-11-10 23:35:12 +0300

    handling failure and loading state.

--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weblog/core/common/widgets/loader.dart';
 import 'package:weblog/core/theme/app_pallete.dart';
+import 'package:weblog/core/utils/show_snakbar.dart';
 import 'package:weblog/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:weblog/features/auth/presentation/pages/sign_in_page.dart';
 import 'package:weblog/features/auth/presentation/widgets/auth_field.dart';
@@ -32,67 +34,79 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: formkey,
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Sign Up.',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 30),
-              AuthField(hintText: "Name", controller: namecontroller),
-              SizedBox(height: 15),
-              AuthField(hintText: "Email", controller: emailcontroller),
-              SizedBox(height: 15),
-              AuthField(
-                hintText: "Password",
-                controller: passwordcontroller,
-                isObscure: true,
-              ),
-              SizedBox(height: 20),
-              AuthGradientButton(
-                buttonText: "Sign Up.",
-                onPressed: () {
-                  if (formkey.currentState!.validate()) {
-                    final email = emailcontroller.text.trim();
-                    final password = passwordcontroller.text.trim();
-                    final name = namecontroller.text.trim();
-
-                    context.read<AuthBloc>().add(
-                      AuthSignUp(email: email, name: name, password: password),
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(SignUpPage.route());
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Don\'t have an account? ',
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: [
-                      TextSpan(
-                        text: 'Sign Up',
-                        style: Theme.of(context).textTheme.titleMedium
-                            ?.copyWith(
-                              color: AppPallete.gradient2,
-                              fontWeight: FontWeight.bold,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+         if(state is AuthFailure){
+          showSnakbar(context, state.message);
+         }
+        },
+        builder: (context, state) {
+          if(state is AuthLoading){
+            return Loader();
+          }
+          return Form(
+              key: formkey,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Sign Up.',
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 30),
+                    AuthField(hintText: "Name", controller: namecontroller),
+                    SizedBox(height: 15),
+                    AuthField(hintText: "Email", controller: emailcontroller),
+                    SizedBox(height: 15),
+                    AuthField(
+                      hintText: "Password",
+                      controller: passwordcontroller,
+                      isObscure: true,
+                    ),
+                    SizedBox(height: 20),
+                    AuthGradientButton(
+                      buttonText: "Sign Up.",
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          final email = emailcontroller.text.trim();
+                          final password = passwordcontroller.text.trim();
+                          final name = namecontroller.text.trim();
+      
+                          context.read<AuthBloc>().add(
+                            AuthSignUp(email: email, name: name, password: password),
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(SignUpPage.route());
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Don\'t have an account? ',
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: [
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: Theme.of(context).textTheme.titleMedium
+                                  ?.copyWith(
+                                    color: AppPallete.gradient2,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                             ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            );
+        },
       ),
     );
   }
