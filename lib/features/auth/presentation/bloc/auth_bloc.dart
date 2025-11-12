0ef8e8933f4b238551d9f16c9ee897f4e3727713
Samuel Ketema <samuelketema01@gmail.com>
2025-11-12 23:20:57 +0300

    new usecase for authenticaion

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weblog/features/auth/domain/entities/user.dart';
+import 'package:weblog/features/auth/domain/usecase/user_login.dart';
 import 'package:weblog/features/auth/domain/usecase/user_sign_up.dart';
 
 part 'auth_event.dart';
@@ -8,8 +9,10 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
  final UserSignUp _userSignUp;
-  AuthBloc({required UserSignUp userSignUp})
+ final UserSignIn _userSignIn;
+  AuthBloc({required UserSignUp userSignUp, required UserSignIn userSignIn})
     : _userSignUp = userSignUp,
+    _userSignIn = userSignIn,
       super(AuthInitial()) {
     on<AuthSignUp>((event, emit)async {
       emit(AuthLoading());
@@ -24,5 +27,11 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       res.fold((l)=> emit(AuthFailure(message:l.message))
       , (user)=> emit(AuthSuccess(user: user)));
     });
+   on<AuthSignIn>((event, emit)async {
+     emit(AuthLoading());
+     final res = await _userSignIn(UaserSignInParams(email: event.email, password: event.password));
+     res.fold((l)=> emit(AuthFailure(message:l.message))
+      , (user)=> emit(AuthSuccess(user: user)));
+   },);
   }
 }
