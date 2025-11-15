e36e4fe5842f6d545b1010cf80914ae3eab03526
Samuel Ketema <samuelketema01@gmail.com>
2025-11-15 23:35:03 +0300

    usecase for current user data

---
+++ lib/features/auth/domain/usecase/current_user.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:weblog/core/error/failure.dart';
+import 'package:weblog/core/usecase/usecase.dart';
+import 'package:weblog/features/auth/domain/entities/user.dart';
+import 'package:weblog/features/auth/domain/repositories/auth_repository.dart';
+
+class CurrentUser implements Usecase<User,NoParams>{
+  final AuthRepository authRepository;
+  CurrentUser( this.authRepository);
+  @override
+  Future<Either<Failure, User>> call(NoParams params) async {
+   return await authRepository.currentUser();
+  }
+}
+
