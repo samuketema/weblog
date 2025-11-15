4bd8c86960a76a6ecb1692fa6ddc114525202f4f
Samuel Ketema <samuelketema01@gmail.com>
2025-11-15 23:36:14 +0300

    current user usecase registerd in authbloc

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weblog/core/usecase/usecase.dart';
 import 'package:weblog/features/auth/domain/entities/user.dart';
+import 'package:weblog/features/auth/domain/usecase/current_user.dart';
 import 'package:weblog/features/auth/domain/usecase/user_login.dart';
 import 'package:weblog/features/auth/domain/usecase/user_sign_up.dart';
 
@@ -8,30 +10,55 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
- final UserSignUp _userSignUp;
- final UserSignIn _userSignIn;
-  AuthBloc({required UserSignUp userSignUp, required UserSignIn userSignIn})
-    : _userSignUp = userSignUp,
-    _userSignIn = userSignIn,
-      super(AuthInitial()) {
-    on<AuthSignUp>((event, emit)async {
+  final UserSignUp _userSignUp;
+  final UserSignIn _userSignIn;
+  final CurrentUser _currentUser;
+  AuthBloc({
+    required UserSignUp userSignUp,
+    required UserSignIn userSignIn,
+    required CurrentUser currentUser,
+  }) : _userSignUp = userSignUp,
+       _userSignIn = userSignIn,
+       _currentUser = currentUser,
+       super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
       emit(AuthLoading());
-   final res = await  _userSignUp(
+      final res = await _userSignUp(
         UserSignUpParams(
           name: event.name,
           email: event.email,
           password: event.password,
         ),
       );
-  
-      res.fold((l)=> emit(AuthFailure(message:l.message))
-      , (user)=> emit(AuthSuccess(user: user)));
+
+      res.fold(
+        (l) => emit(AuthFailure(message: l.message)),
+        (user) => emit(AuthSuccess(user: user)),
+      );
     });
-   on<AuthSignIn>((event, emit)async {
-     emit(AuthLoading());
-     final res = await _userSignIn(UaserSignInParams(email: event.email, password: event.password));
-     res.fold((l)=> emit(AuthFailure(message:l.message))
-      , (user)=> emit(AuthSuccess(user: user)));
-   },);
+    on<AuthSignIn>((event, emit) async {
+      emit(AuthLoading());
+      final res = await _userSignIn(
+        UaserSignInParams(email: event.email, password: event.password),
+      );
+      res.fold(
+        (l) => emit(AuthFailure(message: l.message)),
+        (user) {
+          
+          emit(AuthSuccess(user: user));
+        },
+      );
+    });
+    on<AuthIsUserLoggedIn>((event, emit) async {
+      final res = await _currentUser(NoParams());
+      res.fold(
+        (l) => emit(AuthFailure(message: l.message)),
+        (r) {
+          print('this is data');
+          print(r.email);
+          emit(AuthSuccess(user: r));
+        },
+      );
+    });
   }
 }
