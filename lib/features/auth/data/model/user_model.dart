e04faf5a9cadddd7d74b0563fd7147a7fd0da818
Samuel Ketema <samuelketema01@gmail.com>
2025-11-15 23:32:20 +0300

    Copy with method added to get email information of logged in user.

--- lib/features/auth/data/model/user_model.dart
+++ lib/features/auth/data/model/user_model.dart
@@ -9,4 +9,15 @@ class UserModel extends User {
       email: map['email'] ?? " ", 
       id: map['id']?? "");
   }
+   UserModel copyWith({
+    String? name,
+    String? email,
+    String? id,
+  }) {
+    return UserModel(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      id: id ?? this.id,
+    );
+  }
 }
