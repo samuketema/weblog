efd41767d0729816929874c814534eda9f0f4458
Samuel Ketema <samuelketema01@gmail.com>
2025-11-19 23:13:16 +0300

    customizind new block add page

--- lib/blog/presentation/pages/add_new_blog_page.dart
+++ lib/blog/presentation/pages/add_new_blog_page.dart
@@ -1,7 +1,9 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:weblog/core/theme/app_pallete.dart';
 
 class AddNewBlogPage extends StatefulWidget {
-  static route() => MaterialPageRoute(builder: (context)   => AddNewBlogPage());
+  static route() => MaterialPageRoute(builder: (context) => AddNewBlogPage());
   const AddNewBlogPage({super.key});
 
   @override
@@ -11,10 +13,36 @@ class AddNewBlogPage extends StatefulWidget {
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.done_rounded))
-      ],),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.done_rounded))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DottedBorder(
+              options: RectDottedBorderOptions(
+                color: AppPallete.borderColor,
+                dashPattern: [10,4]
+              ),
+              child: Container(
+                color: AppPallete.backgroundColor,
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder_open),
+                    SizedBox(height: 15),
+                    Text("Select your Image", style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
