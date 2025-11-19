3254f310db2ef4c4f49080ca6cda9f78abd46296
Samuel Ketema <samuelketema01@gmail.com>
2025-11-19 23:56:13 +0300

    New Textfield widget for our blogeditor

---
+++ lib/blog/presentation/widgets/blog_editor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BlogEditor extends StatelessWidget {
+  final TextEditingController controller;
+  const BlogEditor({super.key,required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextFormField(
+
+    );
+  }
+}
