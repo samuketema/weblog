61452922c6069799383ed3e8c350593fc67612a3
Samuel Ketema <samuelketema01@gmail.com>
2025-11-20 22:54:37 +0300

    customizing the blog editor.

--- lib/blog/presentation/widgets/blog_editor.dart
+++ lib/blog/presentation/widgets/blog_editor.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class BlogEditor extends StatelessWidget {
+  final hint;
   final TextEditingController controller;
-  const BlogEditor({super.key,required this.controller});
+  const BlogEditor({super.key, required this.controller, required this.hint});
 
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-
+    return TextFormField(
+      maxLines: null,
+      controller: controller,
+      decoration: InputDecoration(hintText: hint),
     );
   }
 }
