a313c8bf06171c8a68c0f768adb18bc59872084d
Samuel Ketema <samuelketema01@gmail.com>
2025-11-24 23:45:53 +0300

    A model for the blog

---
+++ lib/blog/data/models/blog_model.dart
@@ -0,0 +1,13 @@
+import 'package:weblog/blog/domain/entities/blog.dart';
+
+class BlogModel extends Blog {
+  BlogModel({
+    required super.id,
+    required super.posterId,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.topics,
+    required super.updatedAt,
+  });
+}
