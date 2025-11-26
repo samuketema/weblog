20687ad40ca02fa2f69469ecb9d1b6de6c2a2122
Samuel Ketema <samuelketema01@gmail.com>
2025-11-26 23:45:56 +0300

    repository interface and implementation for blog.

--- lib/blog/domain/repository/blog_repositroy.dart
+++ lib/blog/domain/repository/blog_repositroy.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 import 'package:fpdart/fpdart.dart';
 import 'package:weblog/blog/domain/entities/blog.dart';
 import 'package:weblog/core/error/failure.dart';
 
 abstract interface class BlogRepositroy {
-  Future<Either<Failure,Blog>> uploadBlog();
+  Future<Either<Failure,Blog>> uploadBlog({
+    required File image,
+    required String title,
+    required String posterId,
+    required String content,
+    required List<String> topics
+  });
 }
