b93f6f19a224eb2d6af070072e5cafe4dfedccab
Samuel Ketema <samuelketema01@gmail.com>
2025-11-27 23:28:50 +0300

    validation for blog Textfield.

--- lib/blog/presentation/widgets/blog_editor.dart
+++ lib/blog/presentation/widgets/blog_editor.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class BlogEditor extends StatelessWidget {
-  final hint;
+  final String hint;
   final TextEditingController controller;
   const BlogEditor({super.key, required this.controller, required this.hint});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: (value){
+        if (value!.trim().isEmpty) {
+          return "$hint is missing";  
+        }
+        return null;
+      },
       maxLines: null,
       controller: controller,
       decoration: InputDecoration(hintText: hint),
