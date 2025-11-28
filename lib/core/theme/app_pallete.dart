c858911569acea945e0311d2b93cd6a6db95daa0
Samuel Ketema <samuelketema01@gmail.com>
2025-11-28 22:53:13 +0300

    color fix for the entire theme

--- lib/core/theme/app_pallete.dart
+++ lib/core/theme/app_pallete.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppPallete {
   static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
-  static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
-  static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
-  static const Color gradient3 = Color.fromRGBO(255, 159, 124, 1);
+  static const Color gradient1 = Color.fromRGBO(132, 35, 158, 1);
+  static const Color gradient2 = Color.fromRGBO(200, 78, 129, 1);
+  static const Color gradient3 = Color.fromRGBO(197, 118, 89, 1);
   static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
